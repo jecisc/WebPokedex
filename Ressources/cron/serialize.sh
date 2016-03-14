52e4b61eeaa2e512b0491a0d18354008c0c3a23a
CyrilFerlicot <cyril.ferlicot@gmail.com>
2016-03-15 00:23:49 +0100

    add cron script :)

---
+++ Ressources/cron/serialize.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+curl 'http://pokedex.ferlicot.fr/pokedexRequest?serialize'
+cp ../../../../Pharo/cogspurlinuxhtARM/log/server.ston ../
+git add ../server.ston
+git commit -m "nightly update :)"
+git push -v origin master
